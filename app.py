002cb297b2b1761387720d358ef894bbfb76da09
adiandoyo <adi.andoyo@gdn-commerce.com>
2024-12-31 00:29:31 +0700

    add pagination

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
+import math
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -80,22 +81,50 @@ def register():
 @login_required
 def home():
     search = request.args.get('search')
+    page = int(request.args.get('page', 1))  # Current page (default 1)
+    per_page = 10  # Results per page
+    offset = (page - 1) * per_page  # Calculate OFFSET for SQL query
+    
     cur = mysql.connection.cursor()
+    
+    # Count total matching records
+    if search:
+        count_query = "SELECT COUNT(*) FROM history WHERE username LIKE %s OR port LIKE %s"
+        cur.execute(count_query, (f"%{search}%", f"%{search}%"))
+    else:
+        count_query = "SELECT COUNT(*) FROM history"
+        cur.execute(count_query)
+    
+    total_results = cur.fetchone()[0]
+    print(total_results)
+    total_pages = math.ceil(total_results / per_page)
+    
+    # Fetch paginated results
     if search:
         query = """SELECT history.*, users.username FROM history
             LEFT JOIN users ON users.id = history.user_id
             WHERE history.username LIKE %s OR port LIKE %s
+            LIMIT %s OFFSET %s
         """
-        cur.execute(query, (f"%{search}%", f"%{search}%"))
+        cur.execute(query, (f"%{search}%", f"%{search}%", per_page, offset))
     else:
       query = """
           SELECT history.*, users.username FROM history
           LEFT JOIN users ON users.id = history.user_id
+          LIMIT %s OFFSET %s
       """
-      cur.execute(query)
+      cur.execute(query, (per_page, offset))
     history = cur.fetchall()
     cur.close()
-    return render_template('home/home.html', title="Home", user=current_user, histories=history, search=search)
+    return render_template(
+      'home/home.html',
+      title="Home",
+      user=current_user,
+      histories=history,
+      search=search,
+      page=page,
+      total_pages=total_pages
+    )
 
 @app.route('/add-user', methods=['GET', 'POST'])
 @login_required
